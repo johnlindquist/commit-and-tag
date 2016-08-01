10c1a48324eb052d38dd90b4e37e44444332d110
johnlindquist <johnlindquist>
2016-08-01 16:44:46 +0000

    update-bin

--- src/commit-and-tag.js
+++ src/commit-and-tag.js
@@ -53,8 +53,8 @@ async function commitAndTag(
     const commitId = await repo.createCommit("HEAD", authorSig, committerSig, name, oid, [parent]);
     const commit = await Commit.lookup(repo, commitId);
 
-    const branch = await Branch.name(repo);
-    console.log(branch);
+    // const branch = await Branch.name(repo);
+    // console.log(branch);
 
     const tagId = await Tag.create(repo, name, commit, repo.defaultSignature(), name, 1);
 
