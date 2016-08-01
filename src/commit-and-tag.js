048278b56d057ce9c2a99c61552e4d247ae50788
johnlindquist <johnlindquist>
2016-08-01 14:42:18 +0000

    foo

--- src/commit-and-tag.js
+++ src/commit-and-tag.js
@@ -2,32 +2,27 @@ const nodegit = require("nodegit");
 const path = require("path");
 const argv = require('minimist')(process.argv.slice(2));
 
-const {name, author} = argv;
-
-async function commitAndTag() {
-    const repo = await nodegit.Repository.open(path.resolve(process.cwd(), ".git"))
+async function commitAndTag(name, author) {
+    const repo = await nodegit.Repository.open(path.resolve(process.cwd(), ".git"));
     const index = await repo.refreshIndex();
 
     await index.addAll();
     await index.write();
+
     const oid = await index.writeTree();
-    const head = await nodegit.Reference.nameToId(repo, "HEAD")
+    const head = await nodegit.Reference.nameToId(repo, "HEAD");
     const parent = await repo.getCommit(head);
 
-    const authorSig = nodegit.Signature.create(author,
-        author, Math.round(Date.now() / 1000), 0);
-    const committerSig = nodegit.Signature.create(author,
-        author, Math.round(Date.now() / 1000), 0);
+    const authorSig = nodegit.Signature.create(author, author, Math.round(Date.now() / 1000), 0);
+    const committerSig = nodegit.Signature.create(author, author, Math.round(Date.now() / 1000), 0);
 
     const commitId = await repo.createCommit("HEAD", authorSig, committerSig, name, oid, [parent]);
-
-
     const commit = await nodegit.Commit.lookup(repo, commitId);
-
-    const tagId = await nodegit.Tag.create(repo, name, commit, repo.defaultSignature(), name, 1)
-
+    const tagId = await nodegit.Tag.create(repo, name, commit, repo.defaultSignature(), name, 1);
 
     console.log("Tag: ", tagId);
 }
 
-commitAndTag();
+
+const {name, author} = argv;
+commitAndTag(name, author);
